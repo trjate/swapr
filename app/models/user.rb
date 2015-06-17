6bd297f5484a4d5771aab21ad8ee8e1854854fb3
Brit Butler <redline6561@gmail.com>
2015-06-17 11:26:45 -0400

    Add callback for access tokens.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,20 @@ class User < ActiveRecord::Base
   validates :email, uniqueness: true, presence: true
   validates :email, format: { with: EMAIL_REGEX,
                               message: "is not a valid email." }
+
+  before_save :ensure_access_token
+
+  def ensure_access_token
+    if self.access_token.blank?
+      self.access_token = User.generate_token
+    end
+  end
+
+  def self.generate_token
+    token = SecureRandom.hex
+    while User.exists?(access_token: token)
+      token = SecureRandom.hex
+    end
+    token
+  end
 end
