b4f467fc9643ccc4bb937c6d2c4b19544b40da69
Brit Butler <redline6561@gmail.com>
2015-06-23 10:07:58 -0400

    Write our first Rails tests!

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "can't save an empty user" do
+    user = User.new
+    refute user.save
+  end
+
+  test "user emails must be unique" do
+    bob1 = User.new(email: "bob@internet.com", password: "hunter2")
+    bob2 = User.new(email: "bob@internet.com", password: "*******")
+    assert bob1.save
+    refute bob2.save, "Tried to save user with duplicate email"
+  end
 end
