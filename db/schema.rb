bd5334ac1ac0682b383854b07dac841ce12aba2e
Brit Butler <redline6561@gmail.com>
2015-06-23 23:09:44 -0400

    Contriband should be part of a user's curation.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150624030608) do
+ActiveRecord::Schema.define(version: 20150624030629) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20150624030608) do
     t.datetime "contraband_updated_at"
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
+    t.integer  "curation_id"
   end
 
   create_table "curations", force: :cascade do |t|
