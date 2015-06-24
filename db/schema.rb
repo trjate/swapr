4b05af44954015b60885c59e5418e8e1256a045e
Brit Butler <redline6561@gmail.com>
2015-06-23 23:39:50 -0400

    Add name column to contraband.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150624030958) do
+ActiveRecord::Schema.define(version: 20150624033913) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20150624030958) do
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
     t.integer  "curation_id"
+    t.string   "name"
   end
 
   add_index "contrabands", ["user_id"], name: "index_contrabands_on_user_id", using: :btree
